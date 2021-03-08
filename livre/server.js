4a28b259ac2336266fe2fec7f513f79d51eb6b77
ZmanU1 <moghomra@student.42lyon.fr>
2021-03-08 16:17:33 +0100

    error not resolved

---
+++ livre/server.js
@@ -0,0 +1,60 @@
+let express    = require('express')
+let app        = express()
+let bodyParser = require('body-parser')
+// moteur de template
+app.set('view engine', 'ejs')
+
+//Midelware
+
+app.use('/assets', express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Routes
+
+app.get('/', (request, response)=>{
+    response.render('pages/index', {test: 'salut'})
+})
+
+app.post('/', (request, response)=>{
+    if (request.body.message === undefined || request.body.message === ''){
+        response.render('pages/index', {hello: "pas de message detecter "})
+    }
+
+})
+ app.listen(8080)
+
+
+
+
+
+
+
+
+
+
+// let bodyParser = require('body-parser')
+// let session   = require('express-session')
+// // Moteur de template
+// app.set('view engine', 'ejs')
+// // Middleware
+// app.use('/assets', express.static('public'))
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+// app.use(session({
+//     secret: 'keyboard cat',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: false }
+// }))
+// app.use(require('./middlewares/flash'))
+// app.get('/', (req, res) => {
+//     res.render('pages/index')
+// })
+// app.post('/', (req, res) => {
+//     if (req.body == undefined || req.body.message == '') {
+//         req.flash('error', "Vous n'avez pas posté de message")
+//         res.redirect('/')
+//     }
+// })
+// app.listen(8080)
--- server.js
+++ server.js
@@ -3,17 +3,43 @@ let fs = require('fs');
 let url = require('url');
 
 
-http.createServer((request, response) => {
-    // fs.readFile('index.html', (err, data)=>{
-    //     if (err) throw err
-    //         response.writeHead(200, {
-    //     'Content-Type': 'text/html; charset=utf-8'
-    //     }) 
-    //  response.end(data)
-    // })
-    response.writeHead(200)
-    // console.log(url.parse(response.url()))
-    console.log(request.url)
-    response.end('bonjour')
+// http.createServer((request, response) => {
+//     // fs.readFile('index.html', (err, data)=>{
+//     //     if (err) throw err
+//     //         response.writeHead(200, {
+//     //     'Content-Type': 'text/html; charset=utf-8'
+//     //     }) 
+//     //  response.end(data)
+//     // })
+//     response.writeHead(200)
+//     // console.log(url.parse(response.url()))
+//   let hello = url.parse(request.url, true).query
+//   console.log('bonjour :' + hello.name)
+
+//     response.end('bonjour')
   
-}).listen(8080)
+// }).listen(8080)
+
+const EventEmitter = require('events');
+
+let App = {
+    start: function(port){
+        let emiiter = new EventEmitter()
+        let server = http.createServer((request, response)=>{
+            response.writeHead(200, {
+                        'Content-Type': 'text/html; charset=utf-8'
+                        })
+            if (request.url === '/'){
+                emiiter.emit('root', response)
+            }
+            response.end()
+
+        }).listen(port)
+        return emiiter
+    }
+}
+
+let app = App.start(8080)
+app.on('root', (response)=>{
+    response.write('je suis a la racine')
+})
